12c69f87e9bb9ff9a54ac0e722a681b55a0834e8
Кирилл <zoolohol@mail.ru>
2020-05-31 08:58:27 +0300

    1

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,15 @@
 #include <client.h>
 #include <gtest/gtest.h>
 
+
+int main() {
+    std::string username;
+    std::getline(std::cin, username);
+    Client client(username);
+    return 0;
+}
+
+
 TEST(talk_to_srv, Test1) {
   EXPECT_TRUE(true);
 }
